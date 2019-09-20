acdd3400aba079bf2278493659c3970328361d6e
Morgan <lockhart.mb@gmail.com>
2019-09-20 16:18:20 -0400

    Following the Gatsby tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
+// import Container from "../components/container"
+import Layout from "../components/layout"
 
-export default () => <div>Oh hey! Hello world!</div>
+export default () => (
+  <Layout>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </Layout>
+)
